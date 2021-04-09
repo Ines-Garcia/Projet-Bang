83aff9e3fa574584c385e843976dc02d73ad1f14
Inès-Garcia <inesgarcia861@gmail.com>
2021-04-09 09:33:25 +0200

    Ajout de la methode ajouterPDV dans Beer

--- src/main/java/fr/umontpellier/iut/bang/cards/Beer.java
+++ src/main/java/fr/umontpellier/iut/bang/cards/Beer.java
@@ -1,9 +1,16 @@
 package fr.umontpellier.iut.bang.cards;
 
+import fr.umontpellier.iut.bang.Player;
+
 public class Beer extends OrangeCard {
 
     public Beer(int value, CardSuit suit) {
         super("Beer", value, suit);
     }
 
+    public void ajouterPDV(Player p1){
+        if (!p1.isDead()){ //vérif si pdv > 0 (vivant) et pdv < maxpdv
+            p1.incrementHealth(1);
+        }
+    }
 }
