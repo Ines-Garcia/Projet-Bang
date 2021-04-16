cf7b0bcd67e3d6db57e4fd742a7b76d69de33762
Axelle-Donot <axelle.donot@hotmail.fr>
2021-04-16 15:56:48 +0200

    méthode de la carte catling

--- src/main/java/fr/umontpellier/iut/bang/cards/Gatling.java
+++ src/main/java/fr/umontpellier/iut/bang/cards/Gatling.java
@@ -1,9 +1,23 @@
 package fr.umontpellier.iut.bang.cards;
 
+import fr.umontpellier.iut.bang.Player;
+
+import java.util.List;
+
 public class Gatling extends OrangeCard {
 
     public Gatling(int value, CardSuit suit) {
         super("Gatling", value, suit);
     }
 
+    @Override
+    public void playedBy(Player player) {
+        super.playedBy(player);
+        List<Player> joueurs = player.getOtherPlayers();
+        for (Player p : joueurs){
+            while (p!=player){
+                p.decrementHealth(1,player);
+            }
+        }
+    }
 }
