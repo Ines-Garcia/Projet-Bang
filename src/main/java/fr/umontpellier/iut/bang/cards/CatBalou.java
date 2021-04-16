8f22c7432793893fa39f40fa2015f4d97c97f1be
francoisw <willy.francois@etu.umontpellier.fr>
2021-04-16 18:12:47 +0200

    CatBalou erreur

--- src/main/java/fr/umontpellier/iut/bang/cards/CatBalou.java
+++ src/main/java/fr/umontpellier/iut/bang/cards/CatBalou.java
@@ -1,9 +1,31 @@
 package fr.umontpellier.iut.bang.cards;
 
+import fr.umontpellier.iut.bang.Player;
+
+import java.util.List;
+
 public class CatBalou extends OrangeCard {
 //coup de foudre
     public CatBalou(int value, CardSuit suit) {
         super("Cat Balou", value, suit);
     }
 
+    public void playedBy(Player player) {
+            super.playedBy(player);
+            List<Player> PlayerRestant = player.getOtherPlayers();
+            Player playerCible = player.choosePlayer("Séléctionne ta cible", PlayerRestant, false); //choisis la cible
+            //Card cardChoisis = player.chooseCard("Choisis une carte en jeu du joueur cible", playerCible.getInPlay(), true, true);
+            //Erreur sur la ligne du dessus car je veux la liste des BLUECARDS mais il accepte que les CARS ce gros béta
+
+            //playerCible.discardFromHand(cardChoisis); //retire la carte de la main du joueur
+
+            //if(cardChoisis==null){
+
+            //}
+
+        //player.
+            //player.choose("Choisis une carte dans la main du joueur cible :", cartePlayerCibleHand, );
+
+
+    }
 }
