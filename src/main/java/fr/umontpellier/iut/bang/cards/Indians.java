37bba39fae47b57e90aaff244d56e47dc0d0669c
Axelle-Donot <axelle.donot@hotmail.fr>
2021-04-16 17:15:40 +0200

    Début de la carte indians!

--- src/main/java/fr/umontpellier/iut/bang/cards/Indians.java
+++ src/main/java/fr/umontpellier/iut/bang/cards/Indians.java
@@ -1,9 +1,32 @@
 package fr.umontpellier.iut.bang.cards;
 
+import fr.umontpellier.iut.bang.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Indians extends OrangeCard {
 
     public Indians(int value, CardSuit suit) {
         super("Indians!", value, suit);
     }
 
+    @Override
+    public void playedBy(Player player) {
+        super.playedBy(player);
+        List<Player> joueurs = player.getOtherPlayers();
+        List<String> choice = new ArrayList<>();
+        choice.add("Oui");
+        choice.add("Non");
+
+        for (Player p : joueurs){
+            if (p.getHand().contains("Bang")) {
+                if (p.choose("Voulez vous jouer un Bang", choice, true, false) == "") {
+                    p.decrementHealth(1, player);
+                } else {
+                    //retirer la carte bang de la main du joueur
+                }
+            }
+        }
+    }
 }
