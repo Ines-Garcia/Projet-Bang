e4bbdb6865d792c4cc6e8a325c8808f895e7245b
Romain Bessonnaud <romain.bessonnaud@etu.umontpellier.fr>
2021-04-19 00:09:43 +0200

    MAJ BANG tests ok

--- src/main/java/fr/umontpellier/iut/bang/cards/Barrel.java
+++ src/main/java/fr/umontpellier/iut/bang/cards/Barrel.java
@@ -8,44 +8,9 @@ public class Barrel extends BlueCard {
         super("Barrel", value, suit);
     }
 
-    /*public void playedBy(Player player){
-        super.playedBy(player);
-        player.addToInPlay(this); //met sur le plateau
-        Card degainer = player.randomDraw(); //dégaine une carte
-        if (degainer.getSuit()==CardSuit.HEART){ //si la carte degainer est un coeur
-            //appel carte Missed
-            Card missed = new Missed(1,CardSuit.HEART); //creation carte missed
-            player.addToHand(missed); //ajout dans la main pour la jouer
-            player.playFromHand(missed); //joue la carte (ne la met pas sur le plateau)
-            player.removeFromHand(missed); //remove et pas discard car sinon met la carte dans la defausse
-            //quel condition pour enlever une carte bleu de devant le joueur
-            /*
-            while(player.getInPlay.contain(this){
-                if(je prend prend des degats){
-                    appel carte missed
-                    enleve barrel du plateau
-                }
-            }
-
-        }
-        // carte degainer n'est pas un coeur donc rien ne se passe
-    }*/
-
     public void playedBy(Player player){
         super.playedBy(player);
         player.addToInPlay(this); //met sur le plateau
-
-        /*Card degainer = player.randomDraw(); //dégaine une carte
-        while (player.getInPlay().contains(this)){
-            if (degainer.getSuit()==CardSuit.HEART){ //si la carte degainer est un coeur
-                Card missed = new Missed(1,CardSuit.HEART); //creation carte missed
-                player.addToHand(missed); //ajout dans la main pour la jouer
-                player.playFromHand(missed); //joue la carte (ne la met pas sur le plateau)
-                player.removeFromHand(missed); //remove et pas discard car sinon met la carte dans la defausse
-            }
-        }
-        player.removeFromInPlay(this);
-        // carte degainer n'est pas un coeur donc rien ne se passe */
     }
 
     public boolean canPlayFromHand(Player player) {
