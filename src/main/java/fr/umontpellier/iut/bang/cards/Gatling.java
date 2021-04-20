3f1a87d219ba2940611fdb24a048e6a5e4603b42
Romain Bessonnaud <romain.bessonnaud@etu.umontpellier.fr>
2021-04-20 10:45:13 +0200

    gatling test ok

--- src/main/java/fr/umontpellier/iut/bang/cards/Gatling.java
+++ src/main/java/fr/umontpellier/iut/bang/cards/Gatling.java
@@ -24,11 +24,13 @@ public class Gatling extends OrangeCard {
                 if (p.getHand().contains(p.getCardInHand("Missed!"))) {
                     if (p.choose("Voulez vous utiliser un Missed ? ", choice, false, true).equals("")) { //demande si le joueur veux perdre un point de vie
                         p.decrementHealth(1, player);
+
                     } else {
                         p.discardFromHand(p.getCardInHand("Missed!")); //enleve le bang de la main du joueur
                     }
+                }else {
+                    p.decrementHealth(1, player);
                 }
-                p.decrementHealth(1, player);
             }
         }
     }
