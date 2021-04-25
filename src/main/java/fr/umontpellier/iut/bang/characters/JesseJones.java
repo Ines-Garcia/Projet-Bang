c853e22f936e389c8a3a4b1736d885700f233f56
Axelle-Donot <axelle.donot@hotmail.fr>
2021-04-25 11:52:49 +0200

    carte jesse jones

--- src/main/java/fr/umontpellier/iut/bang/characters/JesseJones.java
+++ src/main/java/fr/umontpellier/iut/bang/characters/JesseJones.java
@@ -1,9 +1,31 @@
 package fr.umontpellier.iut.bang.characters;
 
+import fr.umontpellier.iut.bang.Player;
+import fr.umontpellier.iut.bang.cards.Card;
+
+import java.util.*;
+
+import java.util.ArrayList;
+
 public class JesseJones extends BangCharacter {
 
     public JesseJones() {
         super("Jesse Jones", 4);
     }
 
+    @Override
+    public void onStartTurn(Player player) {
+        /*player.drawToHand();
+        List<String> choix = new ArrayList<>();
+        choix.add("p2");
+        choix.add("");
+        if (player.choose("voulez vous tirer dans la main d'un joueur", choix, false, false).equals("")) {
+            player.drawToHand();
+        } else {
+            List<Player> joueurs = player.getOtherPlayers();
+            Player cible = player.choosePlayer("A qui voulez vous prendre une carte", joueurs, false);
+            Card h = cible.removeRandomCardFromHand();  //enlève la carte de la main de la cible
+            player.addToHand(h); // l'ajoute à la main du player
+        }*/
+    }
 }
